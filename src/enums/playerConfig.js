4c1e390c6d42a8283d9a161d6e966aa861a51145
Nahuel Silvera <nahuelsilverafv@gmail.com>
2022-01-05 15:20:42 -0300

    chore: change player config

--- src/enums/playerConfig.js
+++ src/enums/playerConfig.js
@@ -2,7 +2,7 @@ export const playerConfig = {
     youtube: {
       playerVars: { 
           showinfo: 1, 
-          controls: 0,
+          controls: 1,
           origin: 'http://localhost:3000'
         }
     }
